8bbe9a8eb35344ba05fd3c7bfe2a8fb1685ddc35
Prachi Heda <prachiheda@gmail.com>
2024-11-09 17:27:50 -0500

    input validation

--- popup/popup.js
+++ popup/popup.js
@@ -5,21 +5,97 @@ const endDateElement = document.getElementById("endDate");
 const startButton = document.getElementById("startButton"); 
 const stopButton = document.getElementById("stopButton"); 
 
+const runningSpan = document.getElementById("runningSpan"); 
+const stoppedSpan = document.getElementById("stoppedSpan"); 
+
+//error message
+const locationIdError = document.getElementById("locationIdError");
+const startDateError = document.getElementById("startDateError");
+const endDateError = document.getElementById("endDateError");
+
+
+const hideElement = (elem) =>{
+    elem.style.display = 'none'; 
+}
+
+const showElement = (elem) =>{
+    elem.style.display = ''; 
+}
+
+const disableElement = (elem) =>{
+    elem.disabled = true; 
+}
+
+const enableElement = (elem) =>{
+    elem.disabled = false; 
+}
+
+const handleOnStartState = () =>{
+    showElement(runningSpan); 
+    hideElement(stoppedSpan);
+    disableElement(startButton); 
+    enableElement(stopButton); 
+    //disable elements if we are running
+    disableElement(locationIdElement); 
+    disableElement(startDateElement); 
+    disableElement(endDateElement); 
+}
+
+const handleOnStopState = () =>{
+    showElement(stoppedSpan); 
+    hideElement(runningSpan); 
+    disableElement(stopButton); 
+    enableElement(startButton); 
+    //enable elements if we are running
+    enableElement(locationIdElement); 
+    enableElement(startDateElement); 
+    enableElement(endDateElement); 
+}
+
+//helper function to handle input validations
+const performOnStartValidations = () =>{
+    if(!locationIdElement.value){
+        showElement(locationIdError);
+    } else {
+        hideElement(locationIdError); 
+    }
+
+    if(!startDateElement.value){
+        showElement(startDateError);
+    } else {
+        hideElement(startDateError); 
+    }
+
+    if(!endDateElement.value){
+        showElement(endDateError);
+    } else {
+        hideElement(endDateError); 
+    }
+    return locationIdElement.value && startDateElement.value && endDateElement.value; 
+}
+
 // when we start, we save our preferences that were selected in the extension
 //we then send a chrome runtime message that we have started, and our preferences.
 startButton.onclick = () => {
-    const prefs = {
-        locationId: locationIdElement.value, 
-        startDate: startDateElement.value, 
-        endDate: endDateElement.value,
-        tzData: locationIdElement.options[locationIdElement.selectedIndex].getAttribute('data-tz')
+    //validate inputs 
+    const allFieldsValid = performOnStartValidations(); 
+    if(allFieldsValid){
+        handleOnStartState(); 
+        const prefs = {
+            locationId: locationIdElement.value, 
+            startDate: startDateElement.value, 
+            endDate: endDateElement.value,
+            tzData: locationIdElement.options[locationIdElement.selectedIndex].getAttribute('data-tz')
+        }
+        chrome.runtime.sendMessage({ event: 'onStart', prefs })
     }
-    chrome.runtime.sendMessage({ event: 'onStart', prefs })
+    
 
     
 };
 
 stopButton.onclick = () => {
+    handleOnStopState()
     chrome.runtime.sendMessage({ event: 'onStop' })
 };
 
@@ -39,7 +115,12 @@ chrome.storage.local.get(["locationId", "startDate", "endDate", "locations", "is
     if (endDate) {
         endDateElement.value = endDate; 
     }
-    console.log("running status", isRunning)
+    
+    if(isRunning){
+        handleOnStartState()
+    } else {
+        handleOnStopState()
+    }
 });
 
 
